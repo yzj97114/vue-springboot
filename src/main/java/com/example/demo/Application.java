a3b567e00f711220395c5a7cd68d36c0c49a3e53
yyyzj <792826371@qq.com>
2019-10-24 20:51:22 +0800

    springboot后端增删改查

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author 79282
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.User;
+import org.apache.ibatis.annotations.Mapper; 
+
+import java.util.List;
+
+/**
+ * @author 79282
+ */
+@Mapper
+public interface UserMapper {
+
+    public List<User> findUserByName(String userName);
+
+    public List<User> ListUser();
+
+    public List<User> queryPage(Integer startRows);
+
+    public int getRowCount();
+
+    public int insertUser(User user);
+
+    public int delete(int userId);
+
+    public int Update(User user);
+}
